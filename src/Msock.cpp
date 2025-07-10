732e59851a09e9bb07cb0d4116fd9dd98ddbf179
11mushrooms <fhtpaq@gmail.com>
2025-07-10 17:15:38 +0300

    fixes, and optimizations

--- src/Msock.cpp
+++ src/Msock.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstdint>
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -7,8 +9,64 @@
 
 //======= structure for messages =======
 struct Mess {
-  char data[1024]={0};
-  int data_len=0;
+  char* data;
+  uint64_t data_len=0;
+
+  Mess(){
+    this->data=new char[1024+1];
+    this->data_len=1024;
+  }
+
+  Mess (uint64_t size) {
+    if(size>=0){
+      this->data = new char[size+1];
+      this->data_len=size;
+    }
+  }
+
+  //===destructor===
+  ~Mess(){
+    delete[] data;
+  }
+
+  //===copy constructor===
+  Mess(const Mess& other){
+    data=new char[other.data_len+1];
+    data_len=other.data_len;
+    std::copy(other.data, other.data + other.data_len, data);
+  }
+
+  //===copy assign operator===
+  Mess& operator=(const Mess& other){
+    if (this != &other) {
+      delete[] data;
+      data=new char[other.data_len+1];
+      data_len=other.data_len;
+      std::copy(other.data, other.data + other.data_len, data);
+    }
+    return *this;
+  }
+
+  //===move constructor===
+  Mess(Mess&& other) noexcept {
+    data=other.data;
+    data_len=other.data_len;
+    other.data=nullptr;
+  }
+
+  //===move assign operator===
+  Mess& operator=(Mess&& other) noexcept {
+    if (this != &other) {
+      delete[] data;
+      data = other.data;
+      data_len=other.data_len;
+
+      other.data_len=0;
+      other.data=nullptr;
+    }
+    return *this;
+  }
+
 };
 
 
@@ -90,19 +148,30 @@ int connect(Msock* sock){
 };
 
 //=========Send function for Msock==========
-int send(Msock* sock, char* mess, int len){
+int send(Msock* sock, char* mess, uint64_t len){
   return send((sock->cl ? sock->sockfd : sock->clientfd), mess, len, 0);
 };
 
 //=========Recieve function for Msock==========
-Mess recv(Msock* sock, int size){
-  Mess res;
+Mess recv(Msock* sock, uint64_t size){
+  Mess res(size);
   //char* buff= new char[size];
-  res.data_len=read((sock->cl ? sock->sockfd : sock->clientfd), res.data, 1024);
+  res.data_len=read((sock->cl ? sock->sockfd : sock->clientfd), res.data, size);
   if (res.data_len < 0) {
     std::cerr << "can't recieve data!" << std::endl;
   }
   return res;
+};
+
+//=========Recieve function using pointer, for Msock==========
+void recv(Mess* buff, Msock* sock, uint64_t size){
+  //char* buff= new char[size];
+  delete[] buff->data;
+  buff->data=new char[size+1];
+  buff->data_len=read((sock->cl ? sock->sockfd : sock->clientfd), buff->data, size);
+  if (buff->data_len < 0) {
+    std::cerr << "can't recieve data!" << std::endl;
+  }
 };
 
 //=========Close function for Msock==========
